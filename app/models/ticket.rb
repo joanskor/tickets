59af0177b4b2dc7c3cf4ef5d238282a04297e081
Joanna Skorupa <joanna.skorupa@ready4s.pl>
2018-01-10 23:48:50 +0100

    Tickets in events

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -3,4 +3,8 @@ class Ticket < ApplicationRecord
         :length => { :minimum => 6 }
     validates :email_address, :presence => true
     validates :price, :presence => true
+    validates :phone, :presence => true
+    validates :seat_id_seq, :presence => true
+    validates :address, :presence => true
+    belongs_to :event
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180109200648) do
+ActiveRecord::Schema.define(version: 20180110211931) do
 
   create_table "events", force: :cascade do |t|
     t.string "artist"
@@ -30,6 +30,9 @@ ActiveRecord::Schema.define(version: 20180109200648) do
     t.string "email_address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "phone"
+    t.integer "event_id"
+    t.index ["event_id"], name: "index_tickets_on_event_id"
   end
 
 end
