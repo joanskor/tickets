19818412b4ac84e0185553c9f980d3d8b5cc8b9f
Joanna Skorupa <joanna.skorupa@ready4s.pl>
2018-01-10 14:10:14 +0100

    Event list

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'events/index'
+
+  get 'events/new'
+
+  get 'events/create'
+
+  get 'events/show'
+
   resources :tickets
   root :to => "tickets#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
